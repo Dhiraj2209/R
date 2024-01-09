b370649dc79c63d1b737ea0c156c9c4c450242e7
Dhiraj Prajapati <118041529+Dhiraj2209@users.noreply.github.com>
2024-01-10 00:03:24 +0530

    First R File
    Printing Statements, Variable, Type of Variable.

---
+++ Learning.R
@@ -0,0 +1,21 @@
+#Printing Method in R
+"dhiruu"
+'Dhiraj'
+print('Dhirajj')
+print("Dhiraj")
+print("sh"); # all are Acceptable.
+# hashtag is used for commenting, In R there is no Multilined Comment.
+
+ab = 10
+.a = 11
+b. = 12
+a.b = 122
+Ab = 111
+dhi <- "Dhiraj"
+abc = "Prajapati"
+class(abc)
+class(b.)
+dhi
+abc
+paste(abc,dhi,.a) #Concating 
+ab
