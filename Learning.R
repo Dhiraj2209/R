80dac7255910142754f2c0cbf79ba54404cfc5fc
Dhiraj Prajapati <118041529+Dhiraj2209@users.noreply.github.com>
2024-01-10 12:42:54 +0530

    Operators in R
    Assignment ,Arithmetic operators

--- Learning.R
+++ Learning.R
@@ -11,11 +11,31 @@ ab = 10
 b. = 12
 a.b = 122
 Ab = 111
-dhi <- "Dhiraj"
-abc = "Prajapati"
-class(abc)
+
+dhi <- "Dhiraj" # assignment operator <- , <<-, -> ,->>
+abc <<- "Prajapati"
+class(abc) #Data type
 class(b.)
-dhi
-abc
-paste(abc,dhi,.a) #Concating 
-ab
+
+x = as.integer(b.) # conversion From one data type to another.
+class(x)
+dhi #printing
+paste(abc,dhi,.a) #Concatnating
+
+# multi line String
+mul="ajdkajk
+aandkadjad
+dadaad"
+
+mul # print in one single horizontal line.
+cat(mul) # print in new lines
+grep("dkajk", mul) # finding substring present in string or not.
+nchar(mul) # total number of character
+
+print(ab +Ab)
+print(ab-Ab)
+print(ab*Ab)
+print(ab/Ab)
+print(Ab%%ab) # modulo operator gives reminder
+print(Ab%/%ab) #integer division gives down nearest roundoff value.
+print(ab^Ab) #power
